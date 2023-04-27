3220c4db360007e35469fd42679e3baf468f88f1
Angel Sangerman <a_armando97@hotmail.com>
2023-04-26 23:45:40 -0600

    Procesos update

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,9 +1,26 @@
-const { exec } = require('child_process');
-exec('ls -la', (err, stdout, sterr) => {
+const { exec, spawn } = require('child_process');
+let comando = 'node ./modulos/consola.js';// 'ls -la';
+
+/* exec(comando, (err, stdout, sterr) => {
     if (err) {
         console.error(err);
         return false;
     }
 
     console.log(stdout);
+}); */
+
+let proceso = spawn('ls', ['-la']);
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', (dato) => {
+    console.log('¿Está muerto?')
+    console.log(proceso.killed);
+    console.log(dato.toString());
+});
+
+proceso.on('exit', () => {
+    console.log('El proceso terminó')
+    console.log(proceso.killed);
 });
